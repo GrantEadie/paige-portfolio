c966b6dcc7cc5e8f76ad4a7b314ae65075b71538
Grant Eadie <granteadie@Grants-iMac-2.local>
2022-10-19 14:06:23 -0700

    fix the slider issue.

--- src/comps/PhotoDetail.js
+++ src/comps/PhotoDetail.js
@@ -1,25 +1,37 @@
 import React, { useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import "swiper/swiper.min.css";
 import _ from "lodash";
+import { ArrowLeft } from "phosphor-react";
 
 const PhotoDetail = ({ docs }) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const initialSlide = useCallback(() => {
     return _.findIndex(docs, (img) => `/${img.id}` === location.pathname, 0);
   }, []);
-  console.log(initialSlide);
   return (
-    <div className="photo-detail-root">
-      <Swiper initialSlide={initialSlide()}>
+    <>
+      <Swiper
+        className="photo-detail-root"
+        initialSlide={initialSlide()}
+        slidesPerView={1}
+      >
+      <ArrowLeft
+        size={20}
+        className="back-arrow"
+        onClick={() => navigate("/")}
+      />
         {docs.map((img) => (
           <SwiperSlide key={img.id}>
-            <img className="photo-detail-img" src={img.url} alt="" />
+            <div className="photo-holder">
+              <img className="photo-detail-img" src={img.url} alt="" />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+    </>
   );
 };
 
